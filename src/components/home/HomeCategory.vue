f9ac8e713a4cc0148aa5fd9eca78ac16cea4c8de
静默 <cau1403090523@gmail.com>
2022-10-11 23:01:56 +0800

    finished css

--- src/components/home/HomeCategory.vue
+++ src/components/home/HomeCategory.vue
@@ -1,9 +1,14 @@
 <template>
-  <ul>
-    <li v-for="category in categoryHeads" :key="category.id">
-      <RouterLink to="/">{{ category.name }}</RouterLink>
+  <ul class="menus">
+    <li v-for="category in categoryHeads" :key="category.id" class="menu">
+      <RouterLink to="/" class="link">{{ category.name }}</RouterLink>
 
-      <RouterLink to="/" v-for="item in category.children" :key="item.id">
+      <RouterLink
+        to="/"
+        v-for="item in category.children"
+        :key="item.id"
+        class="link"
+      >
         {{ item.name }}
       </RouterLink>
     </li>
@@ -19,4 +24,31 @@ const categoryStore = useCategoryStore()
 const categoryHeads = computed(() => categoryStore.categoryHeadsWithTwoChildren)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.menus {
+  width: 25rem;
+  height: 50rem;
+  padding-left: 4rem;
+  background-color: $category-bg-color;
+
+  .menu {
+    height: 5rem;
+    line-height: 5rem;
+
+    .link {
+      margin-right: 0.4rem;
+      font-size: 1.4rem;
+      color: #fff;
+
+      &:first-child {
+        font-weight: 500;
+        font-size: 1.6rem;
+      }
+
+      &:hover {
+        color: $theme-primary-color;
+      }
+    }
+  }
+}
+</style>
