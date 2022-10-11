6944781cbf3a9c1c484903d62cd99a1c576c98b3
静默 <cau1403090523@gmail.com>
2022-10-11 21:27:40 +0800

    add HomeLayout

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("vue3"),
   routes,
 })
 
--- src/views/WebLayout.vue
+++ src/views/WebLayout.vue
@@ -1,9 +1,23 @@
 <template>
-  <GlobalNavHeader></GlobalNavHeader>
-  <ShopHeader></ShopHeader>
-  <ShopFixHeader></ShopFixHeader>
-  <main style="height: 50rem"></main>
-  <GlobalFooter></GlobalFooter>
+  <!-- header start -->
+  <section>
+    <GlobalNavHeader></GlobalNavHeader>
+    <ShopHeader></ShopHeader>
+    <ShopFixHeader></ShopFixHeader>
+  </section>
+  <!-- header end -->
+
+  <!-- home start -->
+  <main>
+    <HomeLayout></HomeLayout>
+  </main>
+  <!-- home end -->
+
+  <!-- footer start -->
+  <section>
+    <GlobalFooter></GlobalFooter>
+  </section>
+  <!-- footer end -->
 </template>
 
 <script setup lang="ts">
@@ -11,6 +25,8 @@ import GlobalNavHeader from "@/components/header/GlobalNavHeader.vue"
 import ShopHeader from "@/components/header/ShopHeader.vue"
 import ShopFixHeader from "@/components/header/ShopFixHeader.vue"
 import GlobalFooter from "@/components/footer/GlobalFooter.vue"
+import HomeLayout from "@/views/home/HomeLayout.vue"
+
 import { useCategoryStore } from "@/store/category"
 
 // 初始化category
