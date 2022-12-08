7a0d1f688d7340cc0dade4320098d651c37bf1d4
静默 <cau1403090523@gmail.com>
2022-12-08 18:55:12 +0800

    remove

--- src/components/home/HomeBrand.vue
+++ src/components/home/HomeBrand.vue
@@ -3,8 +3,16 @@
   <PanelComponent main-title="热门品牌" sub-title="国际经典 品质保证">
     <template #right>
       <div class="btns">
-        <PrevIcon class="btn"></PrevIcon>
-        <NextIcon class="btn"></NextIcon>
+        <PrevIcon
+          class="btn"
+          @click="toggle(-1)"
+          :class="{ disabled: index === 0 }"
+        ></PrevIcon>
+        <NextIcon
+          class="btn"
+          @click="toggle(1)"
+          :class="{ disabled: index === 1 }"
+        ></NextIcon>
       </div>
     </template>
     <div class="transition-wrapper" ref="target">
@@ -21,20 +29,33 @@
             </li>
           </ul>
         </template>
-        <ul class="brand-wrapper" v-else>
-          <li v-for="GoodsBrand in GoodsBrands" :key="GoodsBrand.id">
-            <img :src="GoodsBrand.picture" :alt="GoodsBrand.name" />
-          </li>
-        </ul>
+        <div class="box" v-else>
+          <ul
+            class="brand-wrapper"
+            :style="{ transform: `translateX(${-index * 1240}px)` }"
+          >
+            <li v-for="GoodsBrand in GoodsBrands" :key="GoodsBrand.id">
+              <img :src="GoodsBrand.picture" :alt="GoodsBrand.name" />
+            </li>
+          </ul>
+        </div>
       </Transition>
     </div>
   </PanelComponent>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue"
 import { findGoodsBrand } from "@/api/home"
 import { useLazyData } from "@/composables/useLazyData"
 const { target, result: GoodsBrands } = useLazyData(findGoodsBrand)
+const index = ref(0)
+
+const toggle = (step: number) => {
+  const newIndex = index.value + step
+  if (newIndex < 0 || newIndex > 1) return
+  index.value = newIndex
+}
 </script>
 
 <style lang="scss" scoped>
@@ -47,14 +68,23 @@ const { target, result: GoodsBrands } = useLazyData(findGoodsBrand)
     border-radius: 0.3rem;
     cursor: pointer;
     background-color: $theme-primary-color;
+
+    &.disabled {
+      background-color: #ccc;
+      cursor: not-allowed;
+    }
   }
+}
+
+.box {
+  width: 100%;
+  overflow: hidden;
 }
 .brand-wrapper {
   display: flex;
   flex: 1;
   width: 100%;
   height: 34.5rem;
-  overflow: hidden;
   li {
     margin-right: 10px;
 
