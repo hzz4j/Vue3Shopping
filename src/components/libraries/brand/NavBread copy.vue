c6f2235d463c5beb7869f5fdd134ddfee281f824
静默 <cau1403090523@gmail.com>
2022-12-15 14:00:40 +0800

    udate

---
+++ src/components/libraries/brand/NavBread copy.vue
@@ -0,0 +1,54 @@
+<!-- 
+  面包屑
+  使用方式：
+  <NavBread parentPath="/category/1005000" 
+    parentName="电器">空调</NavBread>
+-->
+<template>
+  <div class="bread-wrapper">
+    <div class="bread-item">
+      <RouterLink to="/" class="bread-item">首页</RouterLink>
+      <NextIcon></NextIcon>
+    </div>
+    <div v-if="parentName">
+      <RouterLink :to="parentPath" class="bread-item">{{
+        parentName
+      }}</RouterLink>
+      <NextIcon></NextIcon>
+    </div>
+    <div class="bread-item">
+      <span><slot></slot></span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { h } from "vue"
+interface Props {
+  parentName: string
+  parentPath: string
+}
+
+withDefaults(defineProps<Props>(), {
+  parentName: "",
+  parentPath: "",
+})
+
+const render = () => h("div", { id: 100 }, "Q10Viking")
+</script>
+
+<style lang="scss" scoped>
+.bread-wrapper {
+  padding: 2.5rem 1rem;
+  font-size: 1.6rem;
+  .bread-item {
+    display: inline-block;
+    color: #666;
+    padding: 0 0.25rem;
+
+    &:hover {
+      color: $theme-primary-color;
+    }
+  }
+}
+</style>
--- src/components/libraries/brand/NavBread.vue
+++ src/components/libraries/brand/NavBread.vue
@@ -1,21 +1,27 @@
-<!-- 面包屑 -->
-<template>
-  <div class="bread-wrapper">
-    <RouterLink to="/" class="bread-item">首页</RouterLink>
-    <NextIcon></NextIcon>
-    <RouterLink to="/" class="bread-item">电器</RouterLink>
-    <NextIcon></NextIcon>
-    <RouterLink to="/" class="bread-item">水壶</RouterLink>
-  </div>
-</template>
+<!-- 
+  面包屑
+  使用方式：
+  <NavBread parentPath="/category/1005000" 
+    parentName="电器">空调</NavBread>
+-->
+<script lang="ts">
+import { h, defineComponent } from "vue"
+
+export default defineComponent({
+  render() {
+    return h("div", "hello")
+  },
+})
+</script>
 
 <style lang="scss" scoped>
 .bread-wrapper {
   padding: 2.5rem 1rem;
   font-size: 1.6rem;
   .bread-item {
+    display: inline-block;
     color: #666;
-    padding: 0 0.5rem;
+    padding: 0 0.25rem;
 
     &:hover {
       color: $theme-primary-color;
